373b13f9dc621052841ff73c063482920a8516dc
lacamibarrera <camibarrera@gmail.com>
2024-07-11 19:32:40 -0400

    se añade footer y sus estilos

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,3 +5,8 @@ document.addEventListener('DOMContentLoaded', function () {
       ride: 'carousel'
     });
   });
+
+  var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'));
+  var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+    return new bootstrap.Tooltip(tooltipTriggerEl);
+  });
